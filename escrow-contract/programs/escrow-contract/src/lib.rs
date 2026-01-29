7a7a5c01822d517c30eda09e81b2192597cd9fb4
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-29 18:26:04 +0530

    addeing init struct

--- escrow-contract/programs/escrow-contract/src/lib.rs
+++ escrow-contract/programs/escrow-contract/src/lib.rs
@@ -1,4 +1,9 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{self, Token},
+    token_interface::{self, Burn, Mint, MintTo, TokenAccount, TokenInterface, TransferChecked},
+};
 
 declare_id!("8n6tXMhBaJ67C6nmWZ71e5voHnygzxLEHNenxVrrnbnm");
 
@@ -12,5 +17,25 @@ pub mod escrow_contract {
     }
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct EscrowStateShape {}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    //signer
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub token_mint: InterfaceAccount<'info, Mint>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+
+    //init function
+    #[account(init,  payer= signer, token::mint = token_mint, token::authority = escrow_state_account, token::token_program = token_program, seeds = [b"token_vault", escrow_state_account.key().as_ref(), token_mint.key().as_ref()], bump )]
+    pub token_vault: InterfaceAccount<'info, TokenAccount>,
+
+    //init function
+    pub escrow_state_account: Account<'info, EscrowStateShape>,
+}
